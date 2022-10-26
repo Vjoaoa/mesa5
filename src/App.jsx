9a561cdc87b12d4be7fcc2e301ea63ce57f9b549
Vjoaoa <jl159456@gmail.com>
2022-10-25 22:48:06 -0300

    aula5

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import './Styles.css';
+
+
 const produtos = [
   {
+    id: 1, 
     nome: "Macbook Air 13 Chip M1 256gb",
     image: "https://m.media-amazon.com/images/I/41O807iqbCL._AC_SX522_.jpg",
     preco: "R$ 7999.00",
   },
   {
+    id: 2,
     nome: "Echo Dot (4ª Geração)",
     image: "https://m.media-amazon.com/images/I/714Rq4k05UL._AC_SL1000_.jpg",
     preco: "R$ 379.00",
   },
   {
+    id: 3,
     nome: "Câmera Ip Sem Fio 360°",
     image: "https://m.media-amazon.com/images/I/51F70OM213S._AC_SL1000_.jpg",
     preco: "R$ 199.00",
   },
   {
+    id: 4,
+    nome: "Fechadura Eletrônica Digital Inteligente Zigbee",
+    image: "https://m.media-amazon.com/images/I/51RXeqMLceL._AC_SL1500_.jpg",
+    preco: "R$ 1599.00",
+  },
+
+  {
+    id: 5,
     nome: "Fechadura Eletrônica Digital Inteligente Zigbee",
     image: "https://m.media-amazon.com/images/I/51RXeqMLceL._AC_SL1500_.jpg",
     preco: "R$ 1599.00",
@@ -25,7 +39,7 @@ function App() {
   return (
     <div>
       <h1>Produtos</h1>
-
+{/* 
       <ul>
         <li>
           <h2>{produtos[0].nome}</h2>
@@ -44,6 +58,16 @@ function App() {
           <img src={produtos[2].image} />
           <h3>{produtos[2].preco}</h3>
         </li>
+      </ul> */}
+
+      <ul>
+        {produtos.map((item) => <li key={item.id}>
+        <h2 >{item.nome} </h2>
+        
+        <img src={item.image}/>
+      
+        <h3 >{item.preco} </h3>
+        </li>)}
       </ul>
     </div>
   );
